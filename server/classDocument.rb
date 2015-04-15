d93f53c7d619c931ca4e3e35cda892efdf4b528d
Frank D <frankd@frankd.info>
2015-04-15 01:44:20 -0400

    A different tact continued

--- server/classDocument.rb
+++ server/classDocument.rb
@@ -227,7 +227,7 @@ class Document < DocumentBase
       begStr = myStr[0..(char)]
       endStr = myStr[(char + 1)..-1]
       if (@data.length > (line + 1))
-        endStr = endStr + @data.fetch(line + 1)
+        endStr = endStr + @data.fetch(line + 1).to_s
       end
       puts "begStr is " + begStr.inspect
       puts "endStr is " + endStr.inspect
