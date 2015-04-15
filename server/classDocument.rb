4b1f62560f129ad4ec6ac74d8bf848b60a473040
Frank <frankd@frank-d.info>
2015-04-14 20:01:38 -0700

    Fix some issues with deleteDataMultiLine

--- server/classDocument.rb
+++ server/classDocument.rb
@@ -258,25 +258,35 @@ class Document < DocumentBase
 		length = data.length
 		deleteDataSingleLine(client, line,data,char,length)
 	end
-	
-	def procMsg_deleteDataMultiLine(client, jsonMsg)
-	  ml = jsonMsg['deleteDataMultiLine']
-	  startChar = ml['startChar'].to_i
-	  startLine = ml['startLine'].to_i
-	  endChar = ml['endChar'].to_i
-	  endLine = ml['endLine'].to_i
-	  lineData = ml['data']
-	  i = startLine
-	  while (i <= endLine)
-	    puts "Compare line removal"
-	    puts @data.fetch(i)
-	    puts lineData.fetch(i - startLine)
-  	    @data.delete_at(i)
-	      i += 1
-	      sendMsg_cDeleteLine(client, @name, i)
-	    end
-	end
-	
+ def procMsg_deleteDataMultiLine(client, jsonMsg)
+    ml = jsonMsg['deleteDataMultiLine']
+    startChar = ml['startChar'].to_i
+    startLine = ml['startLine'].to_i
+    endChar = ml['endChar'].to_i
+    endLine = ml['endLine'].to_i
+    lineData = ml['data']
+    i = startLine
+    while (i < endLine)
+      @data.delete_at(startLine)
+      i += 1
+    end
+    sendMsg_cDeleteDataMultiLine(client, @name, i)
+  end
+
+  def sendMsg_cDeleteDataMultiLine(client, document, ml)
+    ml['document'] = document;
+    ml['sourceUser'] = client.name;
+    @clientReply = {
+      'commandSet' => 'document',
+      'command' => 'deleteDataMultiLine',
+      'targetDocument' => name,
+      'deleteDataMultiLine' => ml,
+      #Temporary, each command should come in with a hash so we can deal with fails like this and rectify them
+    }
+    @clientString = @clientReply.to_json
+    @project.sendToClientsListeningExceptWS(client.websocket, document, @clientString)
+
+  end
 	def sendMsg_cDeleteLine(client, document, line)
     @clientReply = {
       'commandSet' => 'document',
